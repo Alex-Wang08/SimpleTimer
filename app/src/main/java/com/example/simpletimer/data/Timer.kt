312b1d821802dc65500f707046549fb4bc47ebdc
alexwang <xiaojie.wang115@gmail.com>
2022-01-20 16:56:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/simpletimer/data/Timer.kt
@@ -0,0 +1,13 @@
+package com.example.simpletimer.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Timer(
+    @PrimaryKey val id: Int? = null,
+    var label: String = "Timer",
+    var originalTime: Long,
+    var currentTime: Long,
+    var isTimerRunning: Boolean = false
+)
